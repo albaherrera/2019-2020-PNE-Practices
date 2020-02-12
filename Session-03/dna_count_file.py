23071574059829de4de59b5749587888ada92b92
albaherrera <albaherrer@hotmail.es>
2020-02-12 18:54:02 +0100

    Sessions

---
+++ Session-03/dna_count_file.py
@@ -0,0 +1,9 @@
+with open('dna.txt.py', 'r') as f:
+    for line in f:
+        index = 0
+        while index < len("dna.txt.py"):
+            print(len("dna.txt.py"))
+            break
+
+
+    f.close()
