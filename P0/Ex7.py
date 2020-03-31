616d80883167de95fa8fa0ff4537435fc52f82f6
albaherrera <albaherrer@hotmail.es>
2020-03-31 19:19:28 +0200

    Practice 0

--- P0/Ex7.py
+++ P0/Ex7.py
@@ -3,7 +3,7 @@ all_bases = ["A","C","G","T"]
 folder ="../Session-04/"
 filename = "U5"
 print("-----| Exercise 7 |------")
-seq = (seq_read_fasta(folder+filename))
-print("frag",(seq_read_fasta(folder+filename)[:20]))
-print("comp",(seq_complement(seq)[:20]))
-
+seq= seq_read_fasta(folder + filename)
+print(" Gene", filename)
+print("frag:",(seq[:20]))
+print("comp:",seq_complement(seq[:20]))
--- P0/seq0.py
+++ P0/seq0.py
@@ -15,10 +15,21 @@ def seq_count_base(seq,base):
     return seq.count(base)
 
 def seq_count(seq):
-    dictionary =
+    d = {'A': seq_count_base(seq, 'A'), 'T': seq_count_base(seq, 'T'),
+           'C': seq_count_base(seq, 'C'), 'G': seq_count_base(seq, 'G')}
+    return d
 
 def seq_reverse(seq):
     return seq[::-1]
 
 def seq_complement(seq):
+    all_bases = ["A","C","G","T"]
+    complement = ["T","G","C","A"]
+    dictionary = dict(zip(all_bases, complement))
+    complement_seq = ""
+    for bases in seq:
+         for base, comp in dictionary.items():
+                if bases == base:
+                    complement_seq += comp
+    return complement_seq
 
