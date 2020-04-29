1af382f8cfb1142c0394073289cbd82fbc973d6c
laherrer <la.herrera.2019@alumnos.urjc.es>
2020-04-29 19:59:59 +0200

    Practice 4

---
+++ Session-14/webserver-EX01.py
@@ -0,0 +1,80 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # Processing the request line
+        req = self.requestline.split(" ")
+
+        # Path will be: "/"
+        path = req[1]
+
+        # Path will be all the things after "/"
+        path = path[1:]
+
+        # Contents and status will be defined after the path is introduced
+        contents = ""
+        status = 0
+
+        if path == "":
+            contents = "Welcome to my server"
+            status = 200
+
+        # ERROR
+        else:
+            contents = "Resource not available"
+            status = 404
+
+
+        # Generating the response message depending on the path introduced
+        self.send_response(status)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
